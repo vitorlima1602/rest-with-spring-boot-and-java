049e23379a1997a4a88c16ff3cb938cd62acb834
Vitor Lima <vitor_lima1602@hotmail.com>
2025-08-09 18:23:28 -0300

    Primeiros passo em Java com Spring Boot

---
+++ src/main/java/com/github/vitorlima1602/model/Greeting.java
@@ -0,0 +1,13 @@
+package com.github.vitorlima1602.model;
+
+public record Greeting(long id, String content) {
+
+//    public Greeting(String content) {
+//        this(0, content);
+//    }
+//
+//    public Greeting(long id, String content) {
+//        this.id = id;
+//        this.content = content;
+//    }
+}
---
+++ src/test/java/com/github/vitorlima1602/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.vitorlima1602;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
